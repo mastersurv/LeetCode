52daec9ec1a0098036cbe2e2c4172d7370e41f39
mastersurv <mastersurv.nikiforov@yandex.ru>
2024-02-14 22:03:52 +0300

    romantoint

--- Solution.py
+++ Solution.py
@@ -1,4 +1,39 @@
 class Solution:
+	def romanToInt(self, s: str) -> int:
+		alphabet = {
+			'I': 1,
+			'V': 5,
+			'X': 10,
+			'L': 50,
+			'C': 100,
+			'D': 500,
+			'M': 1000
+		}
+		res = 0
+		flag = False
+		for i in range(len(s) - 1):
+			if flag:
+				flag = False
+				continue
+			if s[i] == 'I' and s[i + 1] in ('V', 'X'):
+				res += alphabet[s[i + 1]] - 1
+				flag = True
+			elif s[i] == 'X' and s[i + 1] in ('L', 'C'):
+				res += alphabet[s[i + 1]] - 10
+				flag = True
+			elif s[i] == 'C' and s[i + 1] in ('D', 'M'):
+				res += alphabet[s[i + 1]] - 100
+				flag = True
+			else:
+				res += alphabet[s[i]]
+		if len(s) != 1:
+			i += 1
+			if not flag:
+				res += alphabet[s[i]]
+		else:
+			res = alphabet[s[0]]
+		return res
+
 	def isPalindrome(self, x: int) -> bool:
 		x = str(x)
 		if x[::-1] == x:
@@ -21,5 +56,6 @@ class Solution:
 
 # if __name__ == '__main__':
 # 	s = Solution()
-# 	res = s.twoSum([3,3], 6)
+# 	res = s.romanToInt('I')
+# 	# res = s.twoSum([3,3], 6)
 # 	print(res)
