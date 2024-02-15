a8893c0d0310e74569b8c9509ee9942664b52bd4
mastersurv <mastersurv.nikiforov@yandex.ru>
2024-02-15 22:27:12 +0300

    mergewoists

--- Solution.py
+++ Solution.py
@@ -1,4 +1,34 @@
+import typing
+
+
+class ListNode:
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+
 class Solution:
+	def mergeTwoLists(self, list1: typing.Optional[ListNode],
+	                  list2: typing.Optional[ListNode]) -> typing.Optional[ListNode]:
+		res = ListNode(0)
+		current = res
+
+		while list1 and list2:
+			if list1.val < list2.val:
+				current.next = list1
+				list1 = list1.next
+			else:
+				current.next = list2
+				list2 = list2.next
+			current = current.next
+
+		if list1:
+			current.next = list1
+		else:
+			current.next = list2
+
+		return res.next
+
 	def romanToInt(self, s: str) -> int:
 		alphabet = {
 			'I': 1,
@@ -54,8 +84,9 @@ class Solution:
 		return answer
 
 
-# if __name__ == '__main__':
-# 	s = Solution()
-# 	res = s.romanToInt('I')
-# 	# res = s.twoSum([3,3], 6)
-# 	print(res)
+if __name__ == '__main__':
+	s = Solution()
+	res = s.mergeTwoLists([1,2,4], [1,3,4])
+	# res = s.romanToInt('I')
+	# res = s.twoSum([3,3], 6)
+	print(res)
