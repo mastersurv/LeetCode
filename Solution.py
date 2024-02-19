c6f69784b025606fd0173854b0da29f703e9b7d6
mastersurv <mastersurv.nikiforov@yandex.ru>
2024-02-19 14:36:10 +0300

    removeElement

--- Solution.py
+++ Solution.py
@@ -8,6 +8,16 @@ class ListNode:
 
 
 class Solution:
+	def removeElement(self, nums: List[int], val: int) -> int:
+		i = 0
+		while i < len(nums):
+			if nums[i] == val:
+				nums.pop(i)
+			else:
+				i += 1
+		return len(nums)
+
+
 	def removeDuplicates(self, nums: list[int]) -> int:
 		if not nums:
 			return 0
