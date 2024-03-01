87c286028abdde1093dc4d33c47e744a9494da99
mastersurv <mastersurv.nikiforov@yandex.ru>
2024-03-01 23:30:25 +0300

    majorityElement

--- Solution.py
+++ Solution.py
@@ -8,6 +8,21 @@ class ListNode:
 
 
 class Solution:
+	def majorityElement(self, nums: List[int]) -> int:
+		count = 0
+		candidate = None
+
+		for num in nums:
+			if count == 0:
+				candidate = num
+				count = 1
+			elif num == candidate:
+				count += 1
+			else:
+				count -= 1
+
+		return candidate
+
 	def climbStairs(self, n: int) -> int:
 		if n == 1:
 			return 1
