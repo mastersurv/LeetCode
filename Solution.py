8c26344e2a824c5ba323770df5ef6d6b8f97db79
mastersurv <mastersurv.nikiforov@yandex.ru>
2024-03-06 23:36:23 +0300

    merge

--- Solution.py
+++ Solution.py
@@ -8,6 +8,29 @@ class ListNode:
 
 
 class Solution:
+	def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+		"""
+		Do not return anything, modify nums1 in-place instead.
+		"""
+		i = 0
+		j = 0
+		l = 0
+		nums1_copy = nums1[:]
+		while l < m + n:
+			if j == n or n == 0:
+				nums1[l] = nums1_copy[i]
+				i += 1
+			elif i == m:
+				nums1[l] = nums2[j]
+				j += 1
+			elif nums1_copy[i] > nums2[j]:
+				nums1[l] = nums2[j]
+				j += 1
+			else:
+				nums1[l] = nums1_copy[i]
+				i += 1
+			l += 1
+
 	def isIsomorphic(self, s: str, t: str) -> bool:
 		if len(s) != len(t):
 			return False
