d797fa8e1dffd7c9c3c663090197d10a87ce0437
mastersurv <mastersurv.nikiforov@yandex.ru>
2024-03-08 11:00:20 +0300

    generate

--- Solution.py
+++ Solution.py
@@ -8,6 +8,24 @@ class ListNode:
 
 
 class Solution:
+	def generate(self, numRows: int) -> List[List[int]]:
+		if numRows <= 0:
+			return []
+
+		triangle = [[1]]
+
+		for i in range(1, numRows):
+			row = [1]
+
+			for j in range(1, i):
+				row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+			row.append(1)
+
+			triangle.append(row)
+
+		return triangle
+
 	def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 		"""
 		Do not return anything, modify nums1 in-place instead.
