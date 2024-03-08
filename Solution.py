b62278caa35697d8dcde950267e33749bf0edd9d
mastersurv <mastersurv.nikiforov@yandex.ru>
2024-03-08 11:01:50 +0300

    getow

--- Solution.py
+++ Solution.py
@@ -8,6 +8,23 @@ class ListNode:
 
 
 class Solution:
+	def getRow(self, rowIndex: int) -> List[int]:
+		if rowIndex < 0:
+			return []
+		row = [1]
+
+		for i in range(1, rowIndex + 1):
+			new_row = [1]
+
+			for j in range(1, i):
+				new_row.append(row[j - 1] + row[j])
+
+			new_row.append(1)
+
+			row = new_row
+
+		return row
+
 	def generate(self, numRows: int) -> List[List[int]]:
 		if numRows <= 0:
 			return []
